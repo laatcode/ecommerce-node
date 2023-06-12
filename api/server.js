8215bc3981bc8b00b09bc529b7b5062ecd091ad0
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2023-06-12 16:05:53 -0500

    Configurations to deploy in vercel

--- api/server.js
+++ api/server.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 const routes = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => res.send('server running'))
+app.get('/api', (req, res) => res.send('server running'))
 routes(app)
 
 app.use(errorHandler)
